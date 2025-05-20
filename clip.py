c71c252423f55dccd4a5d7136af2e30dc0584213
Wenfei Qi <wenfeiqi@usc.edu>
2025-05-20 16:43:21 +0800

    add dataset and implement preprocess; implement a simple clip model

--- clip.py
+++ clip.py
@@ -1,3 +1,22 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
+from img_encoding import ImgEncoding
+from text_encoding import TextEncoding
+
+
+class CLIP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.imgEncoder = ImgEncoding()
+        self.textEncoder = TextEncoding()
+    def forward(self, img, txt):
+        imgEmb = self.imgEncoder(img)
+        txtEmb= self.textEncoder(txt)
+        return imgEmb @ txtEmb.T
+    
+if __name__ == '__main__':
+    clip = CLIP()
+    img = torch.randn(5, 1, 28, 28)
+    txt = torch.randint(0, 10, (5,))
+    logits = clip(img, txt)
+    print(logits.shape)
